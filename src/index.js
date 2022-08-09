fbf3fa3c0b8cae43656c5555b94912874778345e
SamLinux <oxedom@gmail.com>
2022-08-09 15:38:15 +0300

    set up nav and footer, added eventListners to the btns and binded them to the handleClick function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
+import './style.css';
+//Hook to Inject Content
 let content = document.getElementById('content')
+
+//Creations of NavHeader
 let header = document.createElement('header')
 let nav = document.createElement('nav')
+nav.classList.add('nav-bar', 'centerFlex')
+//Creation of Btns
 let homeBtn = document.createElement('div')
 let menuBtn = document.createElement('div')
 let contactBtn =document.createElement('div')
 let arrOfBtns = [homeBtn,contactBtn,menuBtn]
-import './style.css';
+    homeBtn.innerText = 'Home'
+    menuBtn.innerText = 'Menu'
+    contactBtn.innerText = 'Contact'
+    arrOfBtns.forEach(btn => { 
+         btn.classList.add('nav-btn')
+        btn.addEventListener('click', e => {handleClick(e)})
+        } )
+    nav.append(homeBtn,menuBtn,contactBtn)
 
+//Creation of Footer;
+let footer = document.createElement('footer')
+let spanOne = document.createElement('span')
+let spanTwo = document.createElement('span')
+let spanThree = document.createElement('span')
+footer.append(spanOne,spanTwo,spanThree)
+footer.classList.add('footer', 'centerFlex')
 
-arrOfBtns.forEach(btn => { 
-    btn.classList.add('nav-btn')
-    btn.innerText = 'lorem'}
-    
-    )
-nav.append(homeBtn,menuBtn,contactBtn)
-
+//Adds nav-btn class to each btn
 header.append(nav)
 content.append(header)
+content.append(footer)
 
+
+//HandleClick 
+function handleClick(e){}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  //Dev mode
 mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },{
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
+    
     ],
   },
 };
