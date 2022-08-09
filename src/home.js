792a8432f127e6ebf8056e712eb124837f90e2df
SamLinux <oxedom@gmail.com>
2022-08-09 16:43:38 +0300

    homepage up and running

--- src/home.js
+++ src/home.js
@@ -1,8 +1,22 @@
 
-const mainContainer = document.createElement('main')
-const header = document.createElement('header')
-const section = document.createElement('section')
-const article_summary = document.createElement('article')
-const article_hours =  document.createElement('article')
-const article_location = document.createElement('article')
+export default function home() {
+    
+    const content = document.createElement('div')
+    const mainContainer = document.createElement('main')
+    const header = document.createElement('header')
+    const section = document.createElement('section')
+    const article_summary = document.createElement('article')
+    const article_hours =  document.createElement('article')
+    const article_location = document.createElement('article')
+
+    content.innerText = 'we got mail'
+    content.append(mainContainer,header)
+
+
+    console.log('I get called from home.js!');
+    return content
+    
+  }
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,75 @@
 import './style.css';
-//Hook to Inject Content
-let content = document.getElementById('content')
-
-//Creations of NavHeader
-let header = document.createElement('header')
-let nav = document.createElement('nav')
-nav.classList.add('nav-bar', 'centerFlex')
-//Creation of Btns
-let homeBtn = document.createElement('div')
-let menuBtn = document.createElement('div')
-let contactBtn =document.createElement('div')
-let arrOfBtns = [homeBtn,contactBtn,menuBtn]
-    homeBtn.innerText = 'Home'
-    menuBtn.innerText = 'Menu'
-    contactBtn.innerText = 'Contact'
-    arrOfBtns.forEach(btn => { 
-         btn.classList.add('nav-btn')
-        btn.addEventListener('click', e => {handleClick(e)})
-        } )
-    nav.append(homeBtn,menuBtn,contactBtn)
-
-//Creation of Footer;
-let footer = document.createElement('footer')
-let spanOne = document.createElement('span')
-let spanTwo = document.createElement('span')
-let spanThree = document.createElement('span')
-footer.append(spanOne,spanTwo,spanThree)
-footer.classList.add('footer', 'centerFlex')
-
-//Adds nav-btn class to each btn
-header.append(nav)
-content.append(header)
-content.append(footer)
+import home from './home'
+import menu from './home'
+import contact from './home'
 
 
-//HandleClick 
-function handleClick(e){}
+let dom = function() {
+    
+    //Hook to Inject Content
+    let content = document.getElementById('content')
+
+    //Creations of NavHeader
+    let header = document.createElement('header')
+    let nav = document.createElement('nav')
+    nav.classList.add('nav-bar', 'centerFlex')
+    //Creation of Btns
+    let homeBtn = document.createElement('div')
+    let menuBtn = document.createElement('div')
+    let contactBtn =document.createElement('div')
+    let arrOfBtns = [homeBtn,menuBtn, contactBtn]
+        homeBtn.innerText = 'Home'
+        menuBtn.innerText = 'Menu'
+        contactBtn.innerText = 'Contact'
+        //Giving Btns ID's from 1-3, Class of nav-btn and BindingHandleClick
+
+        for (let index = 0; index < 3; index++) {
+            arrOfBtns[index].setAttribute('id', index+1)
+            arrOfBtns[index].classList.add('nav-btn')
+            arrOfBtns[index].addEventListener('click', e => {handleClick(e)})
+        }
+        
+        nav.append(homeBtn,menuBtn,contactBtn)
+   
+    let main = document.createElement('main')    
+    //Creation of Footer;
+    let footer = document.createElement('footer')
+    let spanOne = document.createElement('span')
+    let spanTwo = document.createElement('span')
+    let spanThree = document.createElement('span')
+    footer.append(spanOne,spanTwo,spanThree)
+    footer.classList.add('footer', 'centerFlex')
+
+    //Appending Default Content to index file 
+    header.append(nav)
+    content.append(header)
+    content.append(main)
+    content.append(footer)
+    
+    //Still need to create contactPage and MenuPage !!
+    const homePage = home()
+    const logic = { selectedPage: homePage}
+    
+   
+    //HandleClick 
+    function handleClick(e, selectedPage){
+        //Removes the Page element before appending new page to dom
+        logic.selectedPage.remove()
+
+        //If Home Button clicked
+        if(parseInt(e.target.id) === 1)  { 
+        logic.selectedPage = homePage
+        main.append(homePage);};
+       
+        //If Home Menu clicked
+       if(parseInt(e.target.id) === 2) 
+
+       { console.log('Menu');};
+
+
+        //If Home Contact clicked
+       if(parseInt(e.target.id) === 3) 
+       { console.log('Contact');};
+    }
+    
+}()
